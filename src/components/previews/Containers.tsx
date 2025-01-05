072e5d5b0189905c6e1fc4548eb9a3cdeac26c4a
postman1year <postman1year@gmail.com>
2025-01-05 18:13:32 +0800

    Add files via upload

--- src/components/previews/Containers.tsx
+++ src/components/previews/Containers.tsx
@@ -1,10 +1,10 @@
 import { Dialog } from '@headlessui/react'
 
-export function PreviewContainer({ children }): JSX.Element {
+export function PreviewContainer({ children }): React.JSX.Element {
   return <Dialog.Panel className="rounded bg-white p-3 shadow-sm dark:bg-gray-900 dark:text-white">{children}</Dialog.Panel>
 }
 
-export function DownloadBtnContainer({ children }): JSX.Element {
+export function DownloadBtnContainer({ children }): React.JSX.Element {
   return (
     <Dialog.Panel className="sticky bottom-0 left-0 right-0 z-10 rounded border-t border-gray-900/10 bg-white bg-opacity-80 p-2 shadow-sm backdrop-blur-md dark:border-gray-500/30 dark:bg-gray-900">
       {children}
