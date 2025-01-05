0886c950d6b46781330f921f726b0a187415e112
postman1year <postman1year@gmail.com>
2025-01-05 18:13:10 +0800

    Add files via upload

--- src/components/SwitchLang.tsx
+++ src/components/SwitchLang.tsx
@@ -1,6 +1,5 @@
 import { Fragment } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Dialog } from '@headlessui/react'
 import { Menu, Transition } from '@headlessui/react'
 
 import { useRouter } from 'next/router'
@@ -8,7 +7,7 @@ import Link from 'next/link'
 import { useCookies, withCookies } from 'react-cookie'
 
 // https://headlessui.dev/react/menu#integrating-with-next-js
-const CustomLink = ({ href, children, as, locale, ...props }): jsx.Element => {
+const CustomLink = ({ href, children, as, locale, ...props }): React.JSX.Element => {
   return (
     <Link href={href} as={as} locale={locale} {...props}>
       {children}
