0205636b02ac3f15a2a099e9583baebd27247476
postman1year <postman1year@gmail.com>
2025-01-09 23:54:18 +0800

    Add files via upload

--- src/components/SwitchLang.tsx
+++ src/components/SwitchLang.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link'
 import { useCookies, withCookies } from 'react-cookie'
 
 // https://headlessui.dev/react/menu#integrating-with-next-js
-const CustomLink = ({ href, children, as, locale, ...props }): React.JSX.Element => {
+const CustomLink = ({ href, children, as, locale, ...props }): JSX.Element => {
   return (
     <Link href={href} as={as} locale={locale} {...props}>
       {children}
